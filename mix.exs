8cd73af3bd6fc4b897d8d34c655965d0464354e8
Gabriel Parizet <gabriel.parizet@wttj.co>
2025-06-17 15:42:36 +0200

    bureaucrat

--- mix.exs
+++ mix.exs
@@ -57,7 +57,9 @@ defmodule SwaggerLibTest.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:bureaucrat, "~> 0.2.10"},
+      {:poison, "~> 3.0"}
     ]
   end
 
